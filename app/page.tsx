9b913eb4d8debe1b21019f21593ea219d01cbb61
fedoroal <Aleksandr.Fedorov@arhs-spikeseed.com>
2024-05-14 14:53:35 +0200

    fix site

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,71 @@
+"use client"; // This must be the first line in the file
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [isHovered, setIsHovered] = useState(false);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#131945] py-12 px-4">
+      <div
+        className={`transform transition-transform duration-500 ${
+          isHovered ? "rotate-180 scale-110" : ""
+        }`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img
+          loading="lazy"
+          srcSet="/logo.svg 100w, /logo.svg 200w, /logo.svg 400w, /logo.svg 800w, /logo.svg 1200w, /logo.svg 1600w, /logo.svg 2000w"
+          className="aspect-square w-[100px] max-md:w-[50px] mb-4"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="text-4xl leading-9 text-center text-white mb-4">
+        BULU
+      </div>
+      <div className="text-2xl font-bold leading-9 text-center text-white mb-4 max-w-md">
+        Aplikasi pertama bagi para pecinta bulu tangkis
+      </div>
+      <div className="text-lg leading-6 text-center text-white mb-4 max-w-md">
+        Tingkatkan kemampuan bermain bulu tangkismu, bagikan momen terbaik
+        bermain dengan teman-teman, kirim video permainanmu untuk direview oleh
+        pelatih berpengalaman
+      </div>
+      <a
+        href="https://forms.gle/qU3wcdj6iS47xktXA"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-16 py-3 mt-4 text-base font-bold tracking-wider leading-5 text-center text-white bg-teal-500 rounded-3xl max-w-xs transition-transform duration-300 transform hover:scale-105 hover:bg-teal-400"
+      >
+        MULAI
+      </a>
+      <div className="flex gap-5 justify-center mt-8">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://forms.gle/qU3wcdj6iS47xktXA"
           target="_blank"
           rel="noopener noreferrer"
+          className="transition-transform duration-300 transform hover:scale-110"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+          <img
+            loading="lazy"
+            srcSet="/apple.svg 100w, /apple.svg 200w, /apple.svg 400w, /apple.svg 800w, /apple.svg 1200w, /apple.svg 1600w, /apple.svg 2000w"
+            className="w-[50px] aspect-square"
+          />
         </a>
-
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://forms.gle/qU3wcdj6iS47xktXA"
           target="_blank"
           rel="noopener noreferrer"
+          className="transition-transform duration-300 transform hover:scale-110"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <img
+            loading="lazy"
+            srcSet="/android.svg 100w, /android.svg 200w, /android.svg 400w, /android.svg 800w, /android.svg 1200w, /android.svg 1600w, /android.svg 2000w"
+            className="w-[50px] aspect-square"
+          />
         </a>
       </div>
-    </main>
+    </div>
   );
 }
